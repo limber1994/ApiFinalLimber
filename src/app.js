a02b8f214d59dc8136807da7b8336d9bddeba75b
limber 888 <limbe.set@gmail.com>
2022-02-09 21:07:06 -0500

    desplegando API v1

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express, { json } from "express";
+import morgan from "morgan";
+import clinicaRoutes from "./routes/clinicas";
+import sucursalRoutes from "./routes/sucursales";
+const app = express();
+//middlewares
+app.use(morgan('dev'));
+app.use(json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content - Type, Accept");
+res.header("Access-Control-Allow-Methods", "HEAD, PUT, POST, GET, DELETE,OPTIONS");
+next();
+});
+//Routes
+app.use('/api/clinicas', clinicaRoutes);
+app.use('/api/sucursales', sucursalRoutes);
+export default app;
